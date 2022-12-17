b84848d48339f5e37e4e606d171fab0eeec81deb
hsuryasr <115404699+hsuryasr@users.noreply.github.com>
2022-12-17 12:59:19 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,167 @@
+﻿//using System;
+//public class HelloWorld
+//{
+//    public static void Main(string[] args)
+//    {
+//        Console.WriteLine("Nested For Loop");
+//        for (int i = 0; i < 4; i++)
+//        {
+//            for (int j = 0; j < 4; j++)
+//            {
+//                Console.WriteLine("when i is {0}", i);
+//                Console.WriteLine("j is {0}", j);
+//            }
+//        }
+//        Console.ReadKey();
+//    }
+//}
+
+
+//internal class Employee
+//{
+//    //Private variable
+//    int EmpId;
+//    //public property
+//    public int EMPID { get; set; }
+//    string EmpName;
+//    public string EMPNAME { get; set; }
+
+
+
+//    //to get the data members should be intialized
+//    //Defalt constructor
+//    public Employee()
+//    {
+//        Console.WriteLine("This is Default constructor for Employee");
+//    }
+//    public Employee(int empid, string empname)
+//    {
+//        this.EMPID = empid;
+//        this.EMPNAME = empname;
+
+
+
+//    }
+
+
+
+//    public void DiplayEmpDetails()
+//    {
+//        Console.WriteLine("The EmpId is  " + EMPID + " " + "Emp Name is " + EMPNAME);
+//    }
+//}
+
+
+
+//class Manager : Employee
+//{
+
+
+
+//    int FoodAllowance;
+//    public int FOODALLOWANCE { get; set; }
+//    public Manager()
+//    {
+//        Console.WriteLine("This is Default constructor for Manager");
+//    }
+//}
+
+
+
+//class ClientEmp
+//{
+//    static void Main()
+//    {
+//        Employee e1 = new Employee(100, "Hema");
+//        e1.DiplayEmpDetails();
+
+
+
+//        //  e1.EmpId;//inaccessible due to projection level
+
+
+
+//        //  e1.EMPID = 100;
+//        // e1.EMPNAME = "Rashmi";
+
+
+
+
+//        //Manager m1 = new Manager();
+//        Console.ReadKey();
+//    }
+//}
+internal class Employee
+{
+    //Private variable
+    int EmpId;
+    //public property
+    public int EMPID { get; set; }
+    string EmpName;
+    public string EMPNAME { get; set; }
+
+
+
+    //to get the data members should be intialized
+    //Defalt constructor
+    public Employee()
+    {
+        Console.WriteLine("This is Default constructor for Employee");
+    }
+    public Employee(int empid, string empname)
+    {
+        this.EMPID = empid;
+        this.EMPNAME = empname;
+
+
+
+    }
+
+
+
+    public void DiplayEmpDetails()
+    {
+        Console.WriteLine("The EmpId is  " + EMPID + " " + "Emp Name is " + EMPNAME);
+    }
+}
+
+
+
+class Manager : Employee
+{
+
+
+
+    int FoodAllowance;
+    public int FOODALLOWANCE { get; set; }
+    public Manager()
+    {
+        Console.WriteLine("This is Default constructor for Manager");
+    }
+}
+
+
+
+class ClientEmp
+{
+    static void Main()
+    {
+        Employee e1 = new Employee(100, "Hema");
+        e1.DiplayEmpDetails();
+
+
+
+        //  e1.EmpId;//inaccessible due to projection level
+
+
+
+        //  e1.EMPID = 100;
+        // e1.EMPNAME = "Rashmi";
+
+
+
+
+        //Manager m1 = new Manager();
+        Console.ReadKey();
+    }
+}
